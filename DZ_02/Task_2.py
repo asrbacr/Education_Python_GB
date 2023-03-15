9f1a3c6596cedd2a179b11fd34a4b590875a1183
Vasiliy Gorilev <89159793029@ya.ru>
2023-03-15 12:46:51 +0300

    Решение задачи 2.1

--- DZ_02/Task_2.py
+++ DZ_02/Task_2.py
@@ -1,30 +1,32 @@
-# 2.2[12]: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. 
-# Петя помогает Кате по математике. 
+# 2.2[12]: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница.
+# Петя помогает Кате по математике.
 # Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
-# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
+# Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.
 # Помогите Кате отгадать задуманные Петей числа.
 
 # Примеры/Тесты:
 # 4 4 -> 2 2
 # 5 6 -> 2 3
 
-summ_number = 5
-proizv_number = 6
-
-# x + y = summ_number }
-# x * y = proizv_number }
-
-# y = summ_number - x
-# x * (summ_number - x) = proizv_number
-
-# y = summ_number - x
-# 1 * x ** x - summ_number * x + proizv_number = 0
-
+summ_number = 374
+proizv_number = 9048
 
 Discrim = summ_number ** 2 - 4 * proizv_number
-print(Discrim)
 
 if Discrim > 0:
-    x1 = (summ_number + Discrim ** 0.5) // 2
-    x2 = (summ_number - Discrim ** 0.5) // 2
-    print(x1, x2)
+    x1 = int((summ_number + (Discrim ** 0.5)) // 2)
+    x2 = int((summ_number - (Discrim ** 0.5)) // 2)
+    y1 = summ_number - x1
+    y2 = summ_number - x2
+
+    if x1 == y2 and x2 == y1:
+        print(f'Задуманы числа {x1} и {y1}')
+    else:
+        print('Решений нет')
+
+elif Discrim == 0:
+    x = int((summ_number + Discrim ** 0.5) // 2)
+    y = summ_number - x
+    print(f'Задуманы числа {x} и {y}')
+else:
+    print('Решений нет')
