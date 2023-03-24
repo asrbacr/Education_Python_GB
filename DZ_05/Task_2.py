158a6d8fc11fc7ecdce62f8837c8fc75626fd8d4
Vasiliy Gorulev <89159793029@ya.ru>
2023-03-24 19:38:06 +0300

    решил задачу 2

--- DZ_05/Task_2.py
+++ DZ_05/Task_2.py
@@ -8,15 +8,10 @@
 # <function_name>(3,0) -> 3
 
 def Summ_number(num_one, num_two):
-    print(num_one, num_two)
     if num_two == 0:
-        print(f'Вошёл в if')
         return num_one
-    print(num_one, num_two)
-    Summ_number(num_one + 1, num_two - 1)
-        # sum = num_one + 1
-        # num_two = num_two - 1
+    return Summ_number(num_one + 1, num_two - 1)
 
-        # return sum
 
-print(Summ_number(0, 2))
+print(Summ_number(3, 4))
+
