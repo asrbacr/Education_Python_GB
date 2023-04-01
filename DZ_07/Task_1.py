d71dedbf0c5888d23eef21c1de2f8d6e7056e84d
Vasiliy Gorulev <89159793029@ya.ru>
2023-04-01 11:01:30 +0300

    Решаю задачу 1

--- DZ_07/Task_1.py
+++ DZ_07/Task_1.py
@@ -23,37 +23,57 @@
 #     <function_name>("Трам-пара-папам-парам-па-пам-пам-па Пум-пурум-пу-пурум-трам-пам-па") -> (False, [{'а': 11}, {'у': 6, 'а': 3}])
 #     <function_name>("Пам-парам-пурум Пум-пурум-карам") -> (True, [{'а': 3, 'у': 2}, {'у': 3, 'а': 2}])
 
-# text = "пара-ра-рам рам-пам-папам па-ра-па-дам"
-text = "пара-ра-рам рам-пуум-пупам па-ре-по-дам"
-
-vowels_rus = {'а': 0, 'у': 0, 'о': 0, 'ы': 0,
-              'и': 0, 'э': 0, 'я': 0, 'ю': 0, 'ё': 0, 'е': 0}
+text = "пара-ра-рам рам-пам-папам па-ра-па-дам"
+# text = "пара-ра-рам рам-пуум-пупам па-ре-по-дам"
 
 list_text = text.split()
 res_list = []
-print(list_text)
-res = 0
-flag = True
+# print(list_text)
+
+vowels_rus = {'а','у','о','ы','и','э','я','ю','ё','е'}
+# for idx in range(len(list_text)):
+result_serch_text = {}
 for el in list_text:
-    # print(f'el = {el}')
-    result_serch_text = {}
-    for idx in range(len(list_text)):
-        for i, k in enumerate(el):
-            res = 0
-            # print(f'i = {i}')
-            # print(f'k = {k}')
-            # if k in result_serch_text:
-            if k in vowels_rus.keys():
-                # if result_serch_text[k] in 
-                # res += 1
-                # res = result_serch_text[k] + 1
-                result_serch_text[k] = res
-                # result_serch_text[k] = res
-    res_list.append((result_serch_text))
+    res = 0
+    for i, k in enumerate(el):
+        if k in vowels_rus:
+            res += 1
+            result_serch_text[el] = res
 
+print(result_serch_text)
 
+res_sum = []
+for i in result_serch_text.items():
+    res_sum.append(i)
 
+print(res_sum)
 
-print(res_list)
+if set(res_sum) == 1:
+    print(True)
+
+# print(result_serch_text)
 
 # print(list_text)
+
+# vowels_rus = {'а': 0, 'у': 0, 'о': 0, 'ы': 0,
+#               'и': 0, 'э': 0, 'я': 0, 'ю': 0, 'ё': 0, 'е': 0}
+
+# # flag = True
+# for el in list_text:
+#     # print(f'el = {el}')
+#     result_serch_text = {}
+#     for idx in range(len(list_text)):
+#         res = 0
+#         for i, k in enumerate(el):
+#             # print(f'i = {i}')
+#             # print(f'k = {k}')
+#             # if k in result_serch_text:
+#             if k in vowels_rus.keys():
+#                 # if result_serch_text[k] in
+#                 res += 1
+#                 # res = result_serch_text[k] + 1
+#                 result_serch_text[k] = res
+#                 # result_serch_text[k] = res
+#     res_list.append((result_serch_text))
+
+# print(result_serch_text)
