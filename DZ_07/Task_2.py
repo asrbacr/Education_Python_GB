494e573d083c6e1a0c7f3989977dd2a5eb166a7d
Vasiliy Gorilev <89159793029@ya.ru>
2023-04-03 13:00:40 +0300

    Решение задачи 2

--- DZ_07/Task_2.py
+++ DZ_07/Task_2.py
@@ -14,10 +14,18 @@
 # 5  10  15  20  25  30
 # 6  12  18  24  30  36
 
-def operation(x,y):
-    for i in range(x):
-        print()
-        for j in range(y):
-            print(f"{(i + 1) ** (j + 1):>4}", end="")
 
-operation(4, 4)
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    matrix_res =  [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+    for i in matrix_res:
+        print(*[f'{x:>4}' for x in i])
+        
+    # for i in range(num_rows):
+    #     print()
+    #     for j in range(num_columns):
+    #         # print(f"{(i + 1) * (j + 1):>4}", end="")
+            
+    #         print(f"{(i + 1) * (j + 1):>4}", end="")
+
+# operation(4, 4)
+print_operation_table(lambda x,y: x*y)
